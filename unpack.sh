0af779a32935a2f8f43f6ad3114fb47398bc005f
haya4 <hayashi.yuu@gmail.com>
2024-12-01 19:13:26 +0900

    Lombok 1.18.36 for Java 17. LombokをアップデートしてJava17 and 21に対応できるようにした

--- unpack.sh
+++ unpack.sh
@@ -1,1 +1,4 @@
-java -Dfile.encoding=utf-8 -jar citygml-osm-jar-with-dependencies.jar unpack
+cp ../../conversion.json .
+java -Dfile.encoding=utf-8 -jar ../../citygml-osm-jar-with-dependencies.jar unpack
+java -Dfile.encoding=utf-8 -jar ../../citygml-osm-jar-with-dependencies.jar pack
+rm *.zip
